d964d771307ebc2a1f063e7dda069db4dbfefc5f
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-14 22:19:29 +0530

    Created product model definition

--- app/models/product.py
+++ app/models/product.py
@@ -1,7 +1,7 @@
 from app.repositories.database import db
 
 class Product (db.model):{
-    id = db.Column(db.Integer, primary_key=True);
+    id = db.Column(db.Integer, primary key=True);
     name = db.Column(db.String(100), unique=True, nullable=False);
     price = db.Column(db.Float, nullable=False);
     description = db.Column(db.String(255), nullable=False;
