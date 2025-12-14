d0e6b34d8de22cb6c0dce31a2ba529e862a948ff
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-14 22:20:47 +0530

    Add configuration module

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,6 +1,8 @@
+# __init__.py
+"""Configuration module."""
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
